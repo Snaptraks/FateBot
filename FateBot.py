000c08405ca478663024f5aa06b89f7b2619b615
François Hardy <snaptraks@gmail.com>
2020-11-03 20:06:35 -0500

    Add on_ready event

--- FateBot.py
+++ FateBot.py
@@ -7,6 +7,17 @@ import config
 class FateBot(commands.Bot):
     """The Bot for the Fate Bound Discord server."""
 
+    async def on_ready(self):
+        permissions = discord.Permissions(permissions=336063568)
+        oauth_url = discord.utils.oauth_url(
+            self.user.id, permissions=permissions)
+        print(
+            f"Logged in as {self.user.name} (ID:{self.user.id}) "
+            f"Use this link to invite {self.user.name}:\n"
+            f"{oauth_url}\n"
+            "--------"
+        )
+
 
 if __name__ == '__main__':
     intents = discord.Intents.all()
