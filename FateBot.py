4c4e7cd336e9e4c347570d29e6d6b8005f0f4337
François Hardy <snaptraks@gmail.com>
2020-11-03 21:49:17 -0500

    First commit for DB

--- FateBot.py
+++ FateBot.py
@@ -1,11 +1,32 @@
+import aiosqlite
 import discord
 from discord.ext import commands
 
 import config
 
 
+async def create_db_connection(db_name):
+    """Create the connection to the SQLite database."""
+
+    return await aiosqlite.connect(
+        db_name, detect_types=1)  # 1: parse declared types
+
+
 class FateBot(commands.Bot):
     """The Bot for the Fate Bound Discord server."""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Create the DB connection and allow for name-based
+        # access of data columns
+        self.db = self.loop.run_until_complete(
+            create_db_connection(kwargs.get('db_name', ':memory')))
+        self.db.row_factory = aiosqlite.Row
+
+    async def close(self):
+        """Subclass the method to close underlying processes."""
+        await self.db.close()
+        await super().close()
 
     async def on_ready(self):
         permissions = discord.Permissions(permissions=336063568)
@@ -26,6 +47,7 @@ if __name__ == '__main__':
         description="Bot for the Fate Bound ESO Guild.",
         command_prefix="&",
         intents=intents,
+        db_name='FateBot.db',
     )
 
     startup_extensions = [
--- cogs/EventESO/eventESO.py
+++ cogs/EventESO/eventESO.py
@@ -1,10 +1,41 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 
 class EventESO(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+        self._create_tables.start()
+
     @commands.command()
     async def trial(self, ctx):
         """Trigger a trial event."""
+
+    @tasks.loop(count=1)
+    async def _create_tables(self):
+        """Create the necessary DB tables if they do not exist."""
+
+        await self.bot.db.execute(
+            """
+            CREATE TABLE IF NOT EXISTS eventeso_event(
+                activation_time TIMESTAMP NOT NULL,
+                channel_id      INTEGER   NOT NULL,
+                creation_time   TIMESTAMP NOT NULL,
+                message_id      INTEGER   NOT NULL
+            )
+            """
+        )
+
+        await self.bot.db.execute(
+            """
+            CREATE TABLE IF NOT EXISTS eventeso_participant(
+                user_id  INTEGER NOT NULL,
+                role     TEXT    NOT NULL,
+                event_id INTEGER NOT NULL,
+                FOREIGN KEY (event_id)
+                    REFERENCES eventeso_event (rowid)
+            )
+            """
+        )
+
+        await self.bot.db.commit()
