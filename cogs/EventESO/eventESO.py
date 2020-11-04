e9a4ec9d63b786cbcb0f763c606c4b7a92447921
François Hardy <snaptraks@gmail.com>
2020-11-04 14:57:50 -0500

    Add Menu for event registration

--- cogs/EventESO/eventESO.py
+++ cogs/EventESO/eventESO.py
@@ -1,4 +1,15 @@
+from datetime import datetime, timedelta
+from dateutil.parser import isoparse
 from discord.ext import commands, tasks
+from . import menus
+
+
+class DateTimeISO(commands.Converter):
+    """Convert a string of ISO time to a datetime object."""
+
+    async def convert(self, ctx, timeiso):
+        dt = isoparse(timeiso)
+        return dt
 
 
 class EventESO(commands.Cog):
@@ -8,8 +19,25 @@ class EventESO(commands.Cog):
         self._create_tables.start()
 
     @commands.command()
-    async def trial(self, ctx):
+    async def trial(self, ctx, *, activation_time: DateTimeISO = None):
         """Trigger a trial event."""
+
+        if activation_time is None:
+            activation_time = datetime.utcnow() + timedelta(hours=1)
+
+        menu = menus.RegistrationMenu(
+            timeout=None,
+            activation_time=activation_time,
+        )
+
+        await menu.start(ctx)
+
+    @trial.error
+    async def trial_error(self, ctx, error):
+        """Error handler for the trial command."""
+
+        if isinstance(error, commands.ConversionError):
+            await ctx.send("Wrong time format. Are you sure it is ISO?")
 
     @tasks.loop(count=1)
     async def _create_tables(self):
